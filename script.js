aa4bd6d6290e787b0e0fe6ddaffea213f7ebfdb1
Saccharin01 <chousik01@gmail.com>
2024-06-12 17:36:51 +0900

    Error Solve
    in MDN, getMonth is start on 0, so use +1 to Calibration

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const {BaseData,assembly,tagComponent} = require('./moduleAssembly')
 const date = new Date();
-let baseData = new BaseData(date.getDate(),date.getMonth(),date.getFullYear())
+let baseData = new BaseData(date.getDate(),date.getMonth()+1,date.getFullYear())
 
 console.log(baseData)
 console.dir(baseData)
@@ -8,3 +8,5 @@ console.log(typeof(baseData))
 console.log(Object.values(baseData))
 console.log(Object.keys(baseData))
 
+console.log(baseData.day)
+console.log(typeof(baseData.day))
