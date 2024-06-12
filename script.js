524d73b7d5beaf3b209539587f2e3ca6ae69f88e
Saccharin01 <chousik01@gmail.com>
2024-06-12 17:26:10 +0900

    Error
    month is not correct.

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const date = new Date();
 let baseData = new BaseData(date.getDate(),date.getMonth(),date.getFullYear())
 
 console.log(baseData)
+console.dir(baseData)
+console.log(typeof(baseData))
+console.log(Object.values(baseData))
+console.log(Object.keys(baseData))
+
