7995fc6d4a756426ccdb4409a0cc65e46f4542e3
Saccharin01 <chousik01@gmail.com>
2024-06-12 17:48:19 +0900

    Logic Check
    how to compare with date.getDate()'s DataType

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ console.dir(baseData)
 console.log(typeof(baseData))
 console.log(Object.values(baseData))
 console.log(Object.keys(baseData))
+console.log(Object.entries(baseData))
 
 console.log(baseData.day)
 console.log(typeof(baseData.day))
+
+console.log(typeof(baseData.day.split("일")[0]))
+
+let something = "";
+something += date.getDate()
+console.log(something)
+console.log(typeof(something))
+
+
+let stateFunc = ()=>{
+  if(baseData.day.split("일")[0] == date.getDate()){
+    console.log('tracking!')
+
+  }
+}
+
+
+stateFunc()
