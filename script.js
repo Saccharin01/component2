e88c3d87a27110b62b0e0c9d72c32ea654fa5909
Saccharin01 <chousik01@gmail.com>
2024-06-12 17:20:43 +0900

    unitTest baseData Creating: Succ

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const {} = require('./moduleAssembly')
+const {BaseData,assembly,tagComponent} = require('./moduleAssembly')
+const date = new Date();
+let baseData = new BaseData(date.getDate(),date.getMonth(),date.getFullYear())
+
+console.log(baseData)
