bc5f4eda6c2fc82c349053a64458feef749d7b79
Saccharin01 <chousik01@gmail.com>
2024-06-12 15:22:13 +0900

    module Assembly created this module will calling inherited module

---
+++ moduleAssembly.js
@@ -0,0 +1,5 @@
+import { Assembly } from "./module/Assembly.js";
+import { NewDate } from "./module/NewDate.js";
+import { TagComponent } from "./module/TagComponent.js";
+
+export {Assembly, NewDate, TagComponent}
