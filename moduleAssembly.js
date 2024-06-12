9868c7dbcfa4e0cb92cb67d2d365eaefc54e3e4c
Saccharin01 <chousik01@gmail.com>
2024-06-12 16:36:03 +0900

    module calling test Compeleted

--- moduleAssembly.js
+++ moduleAssembly.js
@@ -4,6 +4,9 @@
 // export {Assembly, BaseData, TagComponent}
 
 const BaseData = require('./module/BaseData.js')
+const Assembly = require('./module/Assembly.js')
+const TagComponent = require('./module/TagComponent.js')
+
 
 const test = new BaseData(1,2,3)
 console.log(test)
